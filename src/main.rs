0bbcdeaa5705bacefec9eadd604a64253ca49073
Jon Penton <hey@jondpenton.com>
2022-05-22 19:07:15 -0400

    feat: :sparkles: Get `/me` and show deserialized result

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-  println!("Hello, world!");
+use serde::{Deserialize, Serialize};
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let client = reqwest::Client::new();
+  let res = client
+    .get("https://www.pivotaltracker.com/services/v5/me")
+    .header("X-TrackerToken", &env::var("PIVOTAL_TRACKER_API_KEY")?)
+    .send()
+    .await?;
+  let body = res.json::<Me>().await?;
+
+  println!("{:?}", body);
+
+  Ok(())
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Me {
+  email: String,
+  name: String,
 }
