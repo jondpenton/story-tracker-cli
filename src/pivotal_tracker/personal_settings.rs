5bc95b2c52ae00e3728ad23c8b8e9b34381ea275
Jon Penton <hey@jondpenton.com>
2022-05-23 16:53:02 -0400

    fix(pivotal_tracker): :bug: Fixed `HeaderDisplayMode` (de)serialization

--- src/pivotal_tracker/personal_settings.rs
+++ src/pivotal_tracker/personal_settings.rs
@@ -1,20 +1,21 @@
 use serde::{Deserialize, Serialize};
 
-// TODO: Verify file structure is correct.
 #[derive(Serialize, Deserialize, Debug)]
-pub enum HeaderDisplayMode {
-  Collapsed,
-  Expanded,
+pub struct PersonalSettings {
+  /// Controls the state of the header on the project pages.
+  pub header_display_mode: HeaderDisplayMode,
+
+  /// This field is read only.
+  pub kind: String,
+
+  /// Boolean representing whether or not autorefresh should be enabled
+  /// in reports.
+  pub reports_autorefresh: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct PersonalSettings {
-  // Controls the state of the header on the project pages.
-  pub header_display_mode: HeaderDisplayMode,
-
-  // The type of this object: personal_settings. This field is read only.
-  pub kind: String,
-
-  // Boolean representing whether or not autorefresh should be enabled in reports.
-  pub reports_autorefresh: bool,
+#[serde(rename_all = "lowercase")]
+pub enum HeaderDisplayMode {
+  Collapsed,
+  Expanded,
 }
