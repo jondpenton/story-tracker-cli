5eef9b10bf1847bb3bc03cbaa7dc0514cbaa4ea2
Jon Penton <hey@jondpenton.com>
2022-05-23 16:12:32 -0400

    feat: :memo: Added documentation for `pivotal_tracker::Person`

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
   let me = client.get_me().await?;
 
-  println!("{:?}", me.created_at);
+  println!("{:?}", me.person);
 
   Ok(())
 }
