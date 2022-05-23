e7ff39a4e83c8766aa34f4a42412a44e1856547d
Jon Penton <hey@jondpenton.com>
2022-05-23 16:59:32 -0400

    feat(pivotal_tracker): :memo: Added documentation to `TimeZone`

--- src/pivotal_tracker/time_zone.rs
+++ src/pivotal_tracker/time_zone.rs
@@ -1,14 +1,20 @@
 use serde::{Deserialize, Serialize};
 
-// TODO: Verify file structure is correct.
+/// [Pivotal Tracker API](https://www.pivotaltracker.com/help/api/rest/v5#time_zone_resource)
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TimeZone {
-  // The type of this object: time_zone. This field is read only.
+  /// This field is read only.
   pub kind: String,
 
-  // The offset, from UTC, of the time zone. This is a string containing a formatted representation of the time zone offset. First, and optional + or - sign (no sign is equivalent to '+'), then a number of hours, a colon, and a number of minutes. Only valid, internationally-recognized time zone offsets should be used when sending zone information for the client. For example, "-01:03" and "+23:00" are not valid values, even though they match the encoding pattern.
+  /// The offset, from UTC, of the time zone. This is a string containing a
+  /// formatted representation of the time zone offset. First, and optional +
+  /// or - sign (no sign is equivalent to '+'), then a number of hours, a
+  /// colon, and a number of minutes. Only valid, internationally-recognized
+  /// time zone offsets should be used when sending zone information for the
+  /// client. For example, "-01:03" and "+23:00" are not valid values, even
+  /// though they match the encoding pattern.
   pub offset: String,
 
-  // The Olson name for the time zone.
+  /// The Olson name for the time zone.
   pub olson_name: String,
 }
