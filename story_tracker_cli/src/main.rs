3b3bfc96eeddcfe18d96f136f24620511b8505f7
Jon Penton <hey@jondpenton.com>
2022-05-25 09:56:48 -0400

    feat(story_tracker_cli): :sparkles: Finished branch name generation

--- story_tracker_cli/src/main.rs
+++ story_tracker_cli/src/main.rs
@@ -14,21 +14,41 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
   });
   let story = client
     .get_story(GetStoryOptions {
-      id: StoryID(182171003),
+      id: StoryID(181978572),
     })
     .await?;
   let story_name_slug = slugify(story.name.trim());
   let mut story_name_words = story_name_slug.split("-").collect::<Vec<&str>>();
-  let branch_name_parts = vec![format!(
-    "{}/{}",
-    String::from(story.story_type),
-    story_name_words.remove(0)
-  )];
+  let mut branch_name_parts = vec![
+    format!(
+      "{}/{}",
+      String::from(story.story_type),
+      story_name_words.remove(0)
+    ),
+    format!("#{}", story.id.0),
+  ];
 
-  println!(
-    "{:?} {:?} {:?}",
-    branch_name_parts, story_name_words, story.id.0
-  );
+  loop {
+    if story_name_words.len() == 0 {
+      break;
+    }
+
+    let branch_length =
+      branch_name_parts.iter().fold(0, |acc, x| acc + x.len())
+        + branch_name_parts.len()
+        - 1;
+
+    if branch_length + story_name_words[0].len() + 1 > 50 {
+      break;
+    }
+
+    branch_name_parts.insert(
+      branch_name_parts.len() - 1,
+      story_name_words.remove(0).to_string(),
+    );
+  }
+
+  println!("{}", branch_name_parts.join("-"));
 
   Ok(())
 }
