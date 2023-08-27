ecd3587bfc337dd3c259854a130f98b6a083955e
Jon Penton <hey@jondpenton.com>
2023-08-27 11:01:08 -0400

    Feature/185916369 print log messages to stderr (#38)
    * fix(story_tracker_cli): :bug: print debug logs to STDERR
    
    * feat(story_tracker_cli): :sparkles: only print branch name to STDOUT

--- story_tracker_cli/src/subcommands/generate.rs
+++ story_tracker_cli/src/subcommands/generate.rs
@@ -16,18 +16,18 @@ pub async fn run(options: RunOptions<'_>) -> Result<(), Box<dyn Error>> {
     .parse::<StoryID>()
     .expect(&format!("Invalid story ID in {}", story_id));
 
-  println!("Getting story...");
+  eprintln!("Getting story...");
 
   let story = client
     .get_story(GetStoryOptions { id: story_id })
     .await
     .expect("Failed to get story");
 
-  println!("Generating branch name...");
+  eprintln!("Generating branch name...");
 
   let branch_name = branch_name(&story);
 
-  println!("{:#?}", branch_name);
+  println!("{}", branch_name);
 
   Ok(())
 }
