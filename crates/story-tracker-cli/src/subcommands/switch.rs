f4707171f54dfe052cbaa1cb85fce18d02b80223
Jon Penton <hey@jondpenton.com>
2023-09-01 17:47:58 -0400

    refactor: :recycle: cleaned up `get_default_branch()`

--- crates/story-tracker-cli/src/subcommands/switch.rs
+++ crates/story-tracker-cli/src/subcommands/switch.rs
@@ -9,12 +9,12 @@ pub struct RunOptions<'a> {
 }
 
 pub async fn run(_options: RunOptions<'_>) -> Result<(), Box<dyn Error>> {
-	println!("{}", get_default_branch().unwrap());
+	println!("{}", get_default_branch());
 
 	Ok(())
 }
 
-fn get_default_branch() -> Option<String> {
+fn get_default_branch() -> String {
 	let repo = Repository::open_from_env().unwrap();
 	let revspec = repo.revparse_single("refs/remotes/origin/HEAD").unwrap();
 
@@ -25,9 +25,9 @@ fn get_default_branch() -> Option<String> {
 
 	println!("Commit ID: {}", commit_id);
 
-	let branches = repo.branches(Some(BranchType::Remote)).unwrap();
+	let mut branches = repo.branches(Some(BranchType::Remote)).unwrap();
 	let branch_name = branches
-		.filter_map(|branch_tuple| {
+		.find_map(|branch_tuple| {
 			let (branch, _) = branch_tuple.unwrap();
 			let branch_name = branch.name().unwrap().unwrap().to_string();
 
@@ -36,43 +36,14 @@ fn get_default_branch() -> Option<String> {
 			}
 
 			let branch_commit = branch.get().peel_to_commit().unwrap();
-			if branch_commit.id() == commit_id {
-				Some(branch_name)
-			} else {
-				None
+
+			if branch_commit.id() != commit_id {
+				return None;
 			}
+
+			Some(branch_name)
 		})
-		.next()?;
-	Some(branch_name.to_string())
+		.unwrap();
 
-	// let mut remote = repo.find_remote("origin").unwrap();
-	// let mut remote_callbacks = RemoteCallbacks::new();
-
-	// remote_callbacks.credentials(|_url, username_from_url, _allowed_types| {
-	// 	// git2::Cred::ssh_key_from_agent(username_from_url.unwrap_or("git"))
-	// 	git2::Cred::ssh_key(
-	// 		username_from_url.unwrap_or("git"),
-	// 		None,
-	// 		std::path::Path::new("/Users/jondpenton/.ssh/id_ed25519"),
-	// 		None,
-	// 	)
-	// });
-
-	// println!("Connecting to origin...");
-
-	// remote
-	// 	.connect_auth(Direction::Fetch, Some(remote_callbacks), None)
-	// 	.unwrap();
-
-	// println!("Connected to origin. Getting default branch...");
-
-	// let default_branch = remote.default_branch().unwrap();
-
-	// println!("Got default branch. Disconnecting...");
-
-	// remote.disconnect().unwrap();
-
-	// println!("Disconnected.");
-
-	// default_branch.as_str().unwrap().to_string()
+	branch_name.to_string()
 }
