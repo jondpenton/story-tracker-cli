6d42e21e817c37c309e6d3b462aeb991bba0451b
Jon Penton <hey@jondpenton.com>
2024-07-23 22:34:47 -0400

    fix: :bug: fixed build error

--- devenv.nix
+++ devenv.nix
@@ -20,6 +20,7 @@
     ++ lib.optionals pkgs.stdenv.isDarwin [
       pkgs.darwin.apple_sdk.frameworks.CoreFoundation
       pkgs.darwin.apple_sdk.frameworks.Security
+      pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
       pkgs.mktemp
     ];
 
